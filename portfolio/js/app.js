3394f282b71deb50c12f78df304893510d22ff14
yabafre <61940604+yabafre@users.noreply.github.com>
2022-05-04 04:06:04 +0200

    Delete portfolio directory

--- portfolio/js/app.js
+++
@@ -1,9 +0,0 @@
-const header = document.querySelector("header");
-
-/* --------- Sticky Navbar --------- */
-
-function stickyNavbar(){
-    header.classList.toggle("scrolled", window.pageYOffset > 0);
-}
-stickyNavbar();
-window.addEventListener("scrolled", stickyNavbar);
